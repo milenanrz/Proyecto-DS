f6694d0919f2fc792cb5046d7bf233862ed74a53
Milena Narvaez <narvaezmilena084@gmail.com>
2025-04-10 20:23:00 -0500

    added styles operations

--- ActividadPR/db_photographers_operations.py
+++ ActividadPR/db_photographers_operations.py
@@ -15,7 +15,7 @@ async def db_add_photographer(db_session:AsyncSession, name:str, state:bool=None
     return photographer_id
 
 #mostrar por id
-async def db_show_one(photographer_id:int, db_session:AsyncSession):
+async def db_show_one_photographer(photographer_id:int, db_session:AsyncSession):
     query = (select(Photographers).where(Photographers.id == photographer_id))
     result = await db_session.execute(query)
 
@@ -24,7 +24,7 @@ async def db_show_one(photographer_id:int, db_session:AsyncSession):
     return photographer
 
 #mostrar todos los registros
-async def db_show_all(db_session:AsyncSession): #db_session trae la base de datos
+async def db_show_all_photographers(db_session:AsyncSession): #db_session trae la base de datos
     query = (select(Photographers))
     result = await db_session.execute(query)
     photographers = result.scalars().all()
@@ -42,7 +42,7 @@ async def db_modify_state(photographer_id:int, actual_state:bool, db_session:Asy
     return True
 
 #eliminar registro
-async def db_remove(photographer_id:int, db_session:AsyncSession):
+async def db_remove_photographer(photographer_id:int, db_session:AsyncSession):
     result = await db_session.execute(delete(Photographers).where(Photographers.id == photographer_id))
     await db_session.commit()
     if result.rowcount == 0: #si es igual a 0 no elimino nada
