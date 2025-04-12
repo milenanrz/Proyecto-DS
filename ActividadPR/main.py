cd7f3e34a2b1268c12a788ea1777bdc9481e4232
Milena Narvaez <narvaezmilena084@gmail.com>
2025-04-12 18:00:25 -0500

    change some details

--- ActividadPR/main.py
+++ ActividadPR/main.py
@@ -7,12 +7,12 @@ from starlette.responses import JSONResponse
 from models import *
 from contextlib import asynccontextmanager
 from database import Base
-from db_photographers_operations import *
-from db_photographers_connection import AsyncSessionLocal, get_db_session, get_engine
+from photographers_operations import *
+from photographers_connection import AsyncSessionLocal, get_db_session, get_engine
 from typing import Annotated
 from sqlalchemy.ext.asyncio import AsyncSession
-from db_styles_operations import *
-from db_styles_connection import AsyncSessionLocal, get_db_session_, get_engine
+from styles_operations import *
+from styles_connection import AsyncSessionLocal, get_db_session_, get_engine
 
 
 @asynccontextmanager
@@ -28,7 +28,7 @@ app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def root():
-    return {"message": "Hello Photographers"}
+    return {"message": "Hello photographers"}
 
 
 @app.get("/hello/{name}")
@@ -44,33 +44,33 @@ async def raise_exception():
 
 @app.post("/photographer", response_model=dict[str, bool])
 async def add_photographer(photographer:Photographer, db_session:Annotated[AsyncSession,Depends(get_db_session)]):
-    photographer_id = await db_add_photographer(db_session, photographer.name, photographer.state,)
+    photographer_id = await add_photographer(db_session, photographer.name, photographer.state,)
     return {"New photographer":photographer_id}
 
 @app.get("/photographer/{photographer_id}", response_model=PhotographerWithId)
-async def show_photographer_db(photographer_id:int, db_session:Annotated[AsyncSession,Depends(get_db_session)]):
-    photographer = await db_show_one_photographer(photographer_id = photographer_id, db_session = db_session)
+async def show_photographer(photographer_id:int, db_session:Annotated[AsyncSession,Depends(get_db_session)]):
+    photographer = await show_one_photographer(photographer_id = photographer_id, db_session = db_session)
     if photographer is None:
         raise HTTPException(status_code=404, detail="No photographers in the database")
     return photographer
 
 @app.get("/all_photographers", response_model=list[PhotographerWithId])
-async def show_all_photographer_db(db_session:Annotated[AsyncSession,Depends(get_db_session)]):
-    photographers =await db_show_all_photographers(db_session=db_session)
+async def show_all_photographer(db_session:Annotated[AsyncSession,Depends(get_db_session)]):
+    photographers =await show_all_photographers(db_session=db_session)
     if photographers is None:
         raise HTTPException(status_code=404, detail="Photographers not found")
     return photographers
 
 @app.put("/photographer/{photographer_id}")
-async def update_photographer_db(db_session:Annotated[AsyncSession,Depends(get_db_session)], photographer_id:int, actual_state:bool):
-    photographer = await db_modify_state(photographer_id=photographer_id, actual_state=actual_state, db_session=db_session)
+async def update_photographer(db_session:Annotated[AsyncSession,Depends(get_db_session)], photographer_id:int, actual_state:bool):
+    photographer = await modify_state(photographer_id=photographer_id, actual_state=actual_state, db_session=db_session)
     if photographer is None:
         raise HTTPException(status_code=404, detail="Photographer not found")
     return photographer
 
 @app.delete("/photographer/{photographer_id}")
-async def delete_photographer_db(photographer_id:int, db_session:Annotated[AsyncSession,Depends(get_db_session)]):
-    result = await db_remove_photographer(photographer_id=photographer_id, db_session=db_session)
+async def delete_photographer(photographer_id:int, db_session:Annotated[AsyncSession,Depends(get_db_session)]):
+    result = await remove_photographer(photographer_id=photographer_id, db_session=db_session)
     if result is None:
         raise HTTPException(status_code=404, detail="Photographer not found")
     return result
@@ -79,33 +79,33 @@ async def delete_photographer_db(photographer_id:int, db_session:Annotated[Async
 
 @app.post("/photographic_style", response_model=list[str])
 async def add_style(style:PhotographicStyle, db_session:Annotated[AsyncSession,Depends(get_db_session_)]):
-    style_id = await db_add_style(db_session, style.style_name,)
+    style_id = await add_style(db_session, style.style_name,)
     return {"New style":style_id}
 
 @app.get("/photographic_style/{style_id}", response_model=StyleWithId)
-async def show_style_db(style_id:int, db_session:Annotated[AsyncSession,Depends(get_db_session_)]):
-    style = await db_show_one_style(style_id = style_id, db_session = db_session)
+async def show_style(style_id:int, db_session:Annotated[AsyncSession,Depends(get_db_session_)]):
+    style = await show_one_style(style_id = style_id, db_session = db_session)
     if style is None:
         raise HTTPException(status_code=404, detail="No photographic styles in the database")
     return style
 
 @app.get("/all_photographic_styles", response_model=list[StyleWithId])
-async def show_all_styles_db(db_session:Annotated[AsyncSession,Depends(get_db_session_)]):
-    styles =await db_show_all_styles(db_session=db_session)
+async def show_all_styles(db_session:Annotated[AsyncSession,Depends(get_db_session_)]):
+    styles =await show_all_styles(db_session=db_session)
     if styles is None:
         raise HTTPException(status_code=404, detail="Photographic styles not found")
     return styles
 
 @app.put("/photographic_style/{style_id}")
-async def update_style_db(db_session:Annotated[AsyncSession,Depends(get_db_session_)], style_id:int, new_style:str):
-    style = await db_modify_style(style_id=style_id, new_style=new_style, db_session=db_session)
+async def update_style(db_session:Annotated[AsyncSession,Depends(get_db_session_)], style_id:int, new_style:str):
+    style = await modify_style(style_id=style_id, new_style=new_style, db_session=db_session)
     if style is None:
         raise HTTPException(status_code=404, detail="Photographic style not found")
     return style
 
 @app.delete("/photographic_style/{style_id}")
-async def delete_style_db(style_id:int, db_session:Annotated[AsyncSession,Depends(get_db_session_)]):
-    result = await db_remove_style(style_id=style_id, db_session=db_session)
+async def delete_style(style_id:int, db_session:Annotated[AsyncSession,Depends(get_db_session_)]):
+    result = await remove_style(style_id=style_id, db_session=db_session)
     if result is None:
         raise HTTPException(status_code=404, detail="Photographic style not found")
     return result
