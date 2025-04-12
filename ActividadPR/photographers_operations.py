fc17c50a5a0a8ad6545f7572ed2cc93d74d24b46
Milena Narvaez <narvaezmilena084@gmail.com>
2025-04-12 18:02:11 -0500

    change file name

--- ActividadPR/photographers_operations.py
+++ ActividadPR/photographers_operations.py
@@ -4,7 +4,7 @@ from sqlalchemy.future import select
 from database import Photographers
 
 #adicionar un registro
-async def db_add_photographer(db_session:AsyncSession, name:str, state:bool=None,):
+async def add_photographer(db_session:AsyncSession, name:str, state:bool=None,):
     photographer = Photographers(name=name, state=state,)
 
     async with db_session.begin():
@@ -15,7 +15,7 @@ async def db_add_photographer(db_session:AsyncSession, name:str, state:bool=None
     return photographer_id
 
 #mostrar por id
-async def db_show_one_photographer(photographer_id:int, db_session:AsyncSession):
+async def show_one_photographer(photographer_id:int, db_session:AsyncSession):
     query = (select(Photographers).where(Photographers.id == photographer_id))
     result = await db_session.execute(query)
 
@@ -24,7 +24,7 @@ async def db_show_one_photographer(photographer_id:int, db_session:AsyncSession)
     return photographer
 
 #mostrar todos los registros
-async def db_show_all_photographers(db_session:AsyncSession): #db_session trae la base de datos
+async def show_all_photographers(db_session:AsyncSession): #db_session trae la base de datos
     query = (select(Photographers))
     result = await db_session.execute(query)
     photographers = result.scalars().all()
@@ -32,7 +32,7 @@ async def db_show_all_photographers(db_session:AsyncSession): #db_session trae l
     return photographers
 
 #modificar un registro
-async def db_modify_state(photographer_id:int, actual_state:bool, db_session:AsyncSession):
+async def modify_state(photographer_id:int, actual_state:bool, db_session:AsyncSession):
     query = (update(Photographers).where(Photographers.id == photographer_id).values(state=actual_state))
 
     result = await db_session.execute(query)
@@ -42,7 +42,7 @@ async def db_modify_state(photographer_id:int, actual_state:bool, db_session:Asy
     return True
 
 #eliminar registro
-async def db_remove_photographer(photographer_id:int, db_session:AsyncSession):
+async def remove_photographer(photographer_id:int, db_session:AsyncSession):
     result = await db_session.execute(delete(Photographers).where(Photographers.id == photographer_id))
     await db_session.commit()
     if result.rowcount == 0: #si es igual a 0 no elimino nada
--- ActividadPR/styles_operations.py
+++ ActividadPR/styles_operations.py
@@ -4,7 +4,7 @@ from sqlalchemy.future import select
 from database import Styles
 
 #adicionar un registro
-async def db_add_style(db_session:AsyncSession, style_name:str):
+async def add_style(db_session:AsyncSession, style_name:str):
     style = Styles(style_name=style_name,)
 
     async with db_session.begin():
@@ -15,7 +15,7 @@ async def db_add_style(db_session:AsyncSession, style_name:str):
     return style_id
 
 #mostrar por id
-async def db_show_one_style(style_id:int, db_session:AsyncSession):
+async def show_one_style(style_id:int, db_session:AsyncSession):
     query = (select(Styles).where(Styles.id == style_id))
     result = await db_session.execute(query)
 
@@ -24,7 +24,7 @@ async def db_show_one_style(style_id:int, db_session:AsyncSession):
     return style
 
 #mostrar todos los registros
-async def db_show_all_styles(db_session:AsyncSession): #db_session trae la base de datos
+async def show_all_styles(db_session:AsyncSession): #db_session trae la base de datos
     query = (select(Styles))
     result = await db_session.execute(query)
     styles = result.scalars().all()
@@ -32,7 +32,7 @@ async def db_show_all_styles(db_session:AsyncSession): #db_session trae la base 
     return styles
 
 #modificar un registro
-async def db_modify_style(style_id:int, new_style:str, db_session:AsyncSession):
+async def modify_style(style_id:int, new_style:str, db_session:AsyncSession):
     query = (update(Styles).where(Styles.id == style_id).values(style_name=new_style))
 
     result = await db_session.execute(query)
@@ -42,7 +42,7 @@ async def db_modify_style(style_id:int, new_style:str, db_session:AsyncSession):
     return True
 
 #eliminar registro
-async def db_remove_style(style_id:int, db_session:AsyncSession):
+async def remove_style(style_id:int, db_session:AsyncSession):
     result = await db_session.execute(delete(Styles).where(Styles.id == style_id))
     await db_session.commit()
     if result.rowcount == 0: #si es igual a 0 no elimino nada
