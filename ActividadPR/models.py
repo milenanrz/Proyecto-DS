45d78783da53581c399ab982bc091e3a66fd50ae
Milena Narvaez <narvaezmilena084@gmail.com>
2025-04-12 17:36:46 -0500

    change some details

--- ActividadPR/models.py
+++ ActividadPR/models.py
@@ -2,14 +2,14 @@ from pydantic import BaseModel, Field
 #from typing import Optional
 
 class Photographer(BaseModel):
-    name:str = Field(..., min_length=3, max_length=20)
-    condition:bool = Field(...)
+    name:str = Field(min_length=3, max_length=20)
+    state:bool
 
 class PhotographerWithId(Photographer):
     id:int
 
 class PhotographicStyle(BaseModel):
-    style:str = Field(min_length=3, max_length=20)
+    style_name:str = Field(min_length=3, max_length=20)
 
 class StyleWithId(PhotographicStyle):
     id:int
