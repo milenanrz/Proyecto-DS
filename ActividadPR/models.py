61608eb7bc8cbb12c04d3ffb53c1a79eba6c1de0
Milena Narvaez <narvaezmilena084@gmail.com>
2025-04-16 20:46:43 -0500

    added Field() in Photographer class

--- ActividadPR/models.py
+++ ActividadPR/models.py
@@ -1,14 +1,14 @@
 from pydantic import BaseModel, Field
-#from typing import Optional
 
 class Photographer(BaseModel):
     name:str = Field(min_length=3, max_length=20)
-    state:bool
+    state:bool = Field()
 
 class PhotographerWithId(Photographer):
     id:int
 
 class PhotographicStyle(BaseModel):
+    name: str = Field(min_length=3, max_length=20)
     style_name:str = Field(min_length=3, max_length=20)
 
 class StyleWithId(PhotographicStyle):
