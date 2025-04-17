a262024659254846df1bced39270a76dc8f69a70
Milena Narvaez <narvaezmilena084@gmail.com>
2025-04-16 20:42:14 -0500

    added new operation find_style

--- ActividadPR/styles_operations.py
+++ ActividadPR/styles_operations.py
@@ -4,8 +4,8 @@ from sqlalchemy.future import select
 from database import Styles
 
 #adicionar un registro
-async def add_style(db_session:AsyncSession, style_name:str):
-    style = Styles(style_name=style_name,)
+async def add_style(db_session:AsyncSession, name:str, style_name:str):
+    style = Styles(name=name, style_name=style_name,)
 
     async with db_session.begin():
         db_session.add(style)
@@ -48,3 +48,13 @@ async def remove_style(style_id:int, db_session:AsyncSession):
     if result.rowcount == 0: #si es igual a 0 no elimino nada
         return False
     return True
+
+#buscar estilo
+async def find_style(style_name_:str, db_session:AsyncSession):
+    style_name_.lower()
+    query = (select(Styles).where(Styles.style_name == style_name_))
+    result = await db_session.execute(query)
+
+    style = result.scalars().all()
+
+    return style
